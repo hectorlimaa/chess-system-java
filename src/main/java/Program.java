dcd1721b150fbaeaf8b810361a69b973c8bac923
hectorlimaa <hector.limaaa@gmail.com>
2022-01-10 11:18:37 -0300

    Iniciado a implementação de Tabuleiro e peça

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,9 +1,10 @@
+import bordgame.Board;
 import bordgame.Position;
 
 public class Program {
     public static void main(String [] args) {
 
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
+        
     }
 }
