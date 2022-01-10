c89087857b000bcdeda089c03cb0d9120d1ed943
unknown <Qintess@BRNOFFD5B861E74.resource.com.br>
2022-01-10 10:46:30 -0300

    First class: position

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,9 @@
+import bordgame.Position;
+
 public class Program {
     public static void main(String [] args) {
-        System.out.println("Hello world");
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
